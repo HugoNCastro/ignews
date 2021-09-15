57c77a7800d4c83fc22f456b2189a2e682c15e24
Hugo Naaman Castro <hugonaaman@gmail.com>
2021-09-15 09:51:49 -0300

    Adding homepage infos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero} >
           <span>👏 Hey, welcome</span>
-          <h1>News about <br/>the <span>React</span> world.</h1>
+          <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publicactions <br />
             <span>for $9.90 month</span>
